28dbffdbadddcce1ace7e34ba595e01cc3156621
David Dang <ddang.sw@gmail.com>
2016-04-20 13:45:35 -0700

    Add the initial resources needed for this project

---
+++ client/js/wordcraft.js
@@ -0,0 +1,5 @@
+// Client-side code
+/* jshint browser: true, jquery: true, curly: true, eqeqeq: true, forin: true,
+immed: true, indent: 4, latedef: true, newcap: true, nonew: true, quotmark: double,
+undef: true, unused: true, strict: true, trailing: true */
+/* global console: true, io: true, _: true, ko: true, alert: true*/
